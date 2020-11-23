b032620404243751e58f4a2200e03fb0049ab8c5
zhaoshihan <zhao_shihan@foxmail.com>
2020-11-23 20:23:07 +0800

    Update

--- 3/3.cc
+++ 3/3.cc
@@ -2,14 +2,32 @@
 #include "high_precision_computing/float.hh"
 #include "high_precision_computing/math.hh"
 #include "analysis/analysis.hh"
+using std::endl;
 
 Analysis<hp_float> anal;
-static const hp_float dx = 1e-200;
+
+static const hp_float dx = 1e-40;
+static const size_t max_iter = 100;
 hp_float phi(hp_float x);
 hp_float func(hp_float x);
 
 int main() {
-    anal.FindRoot(func, 0.9, 10000, 1e-128).Print(128);
+    for (size_t i = 1; i <= 3; ++i) {
+        if (func(i) < 0.) {
+            hp_float left = anal.FindRoot(func, i - 0.1, max_iter, 1e-32);
+            hp_float right = anal.FindRoot(func, i + 0.1, max_iter, 1e-32);
+            if (left < right) {
+                cout << "For x = " << i << ", the convergence domain is" << endl << "( ";
+                left.Print(32);
+                cout << ", ";
+                right.Print(32);
+                cout << " )" << endl;
+                continue;
+            }
+        }
+        cout << "For x = " << i << ", the equation does not converges." << endl;
+    }
+    return 0;
 }
 
 hp_float phi(hp_float x) {
@@ -17,5 +35,6 @@ hp_float phi(hp_float x) {
 }
 
 hp_float func(hp_float x) {
-    return fabs(anal.D(phi, x, dx)) - 1.;
+    hp_float a = fabs(anal.D(phi, x, dx)) - 1.;
+    return a;
 }
